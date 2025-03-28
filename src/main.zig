63428ab6ad82977b31882b68e42b29131a130730
ILIYA LESANI <iliyalesani@gmail.com>
2025-03-28 21:55:08 +0330

    Resize tabs and buttons

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,10 @@ const c = @cImport({
 
 const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 600;
-const TAB_HEIGHT = 40;
+const TAB_HEIGHT = 30;
 const BUTTON_HEIGHT = 30;
+const BUTTON_WIDTH = 120;
+const TAB_WIDTH = 100;
 
 const Tab = enum {
     system,
@@ -27,20 +29,32 @@ pub fn main() void {
         c.BeginDrawing();
         c.ClearBackground(c.RAYWHITE);
 
-        // Draw tabs
-        const tab_width = WINDOW_WIDTH / 2;
+        // Draw tabs at top left
+        const tab_spacing = 5;
+        const tab_x = 10;
+        const tab_y = 10;
+
+        // System tab
+        if (current_tab == .system) {
+            c.DrawRectangle(tab_x, tab_y, TAB_WIDTH, TAB_HEIGHT, c.LIGHTGRAY);
+        }
         if (c.GuiButton(.{
-            .x = 0,
-            .y = 0,
-            .width = tab_width,
+            .x = tab_x,
+            .y = tab_y,
+            .width = TAB_WIDTH,
             .height = TAB_HEIGHT,
         }, "System")) {
             current_tab = .system;
         }
+
+        // Sound tab
+        if (current_tab == .sound) {
+            c.DrawRectangle(tab_x + TAB_WIDTH + tab_spacing, tab_y, TAB_WIDTH, TAB_HEIGHT, c.LIGHTGRAY);
+        }
         if (c.GuiButton(.{
-            .x = tab_width,
-            .y = 0,
-            .width = tab_width,
+            .x = tab_x + TAB_WIDTH + tab_spacing,
+            .y = tab_y,
+            .width = TAB_WIDTH,
             .height = TAB_HEIGHT,
         }, "Sound")) {
             current_tab = .sound;
@@ -50,33 +64,33 @@ pub fn main() void {
         switch (current_tab) {
             .system => {
                 // System tab content will go here
-                c.DrawText("System Information", 10, TAB_HEIGHT + 10, 20, c.BLACK);
+                c.DrawText("System Information", tab_x, tab_y + TAB_HEIGHT + 20, 20, c.BLACK);
             },
             .sound => {
                 // Sound tab content will go here
-                c.DrawText("Sound Information", 10, TAB_HEIGHT + 10, 20, c.BLACK);
+                c.DrawText("Sound Information", tab_x, tab_y + TAB_HEIGHT + 20, 20, c.BLACK);
             },
         }
 
-        // Draw buttons at the bottom
-        const button_width = 150;
-        const button_spacing = 20;
-        const total_buttons_width = (button_width * 2) + button_spacing;
-        const start_x = (WINDOW_WIDTH - total_buttons_width) / 2;
+        // Draw buttons at bottom right
+        const button_spacing = 10;
+        const button_y = WINDOW_HEIGHT - BUTTON_HEIGHT - 20;
+        const save_x = WINDOW_WIDTH - (BUTTON_WIDTH * 2) - button_spacing - 20;
+        const exit_x = WINDOW_WIDTH - BUTTON_WIDTH - 20;
 
         if (c.GuiButton(.{
-            .x = start_x,
-            .y = WINDOW_HEIGHT - BUTTON_HEIGHT - 20,
-            .width = button_width,
+            .x = save_x,
+            .y = button_y,
+            .width = BUTTON_WIDTH,
             .height = BUTTON_HEIGHT,
         }, "Save Information")) {
             // Save functionality will be implemented later
         }
 
         if (c.GuiButton(.{
-            .x = start_x + button_width + button_spacing,
-            .y = WINDOW_HEIGHT - BUTTON_HEIGHT - 20,
-            .width = button_width,
+            .x = exit_x,
+            .y = button_y,
+            .width = BUTTON_WIDTH,
             .height = BUTTON_HEIGHT,
         }, "Exit")) {
             exit_requested = true;
